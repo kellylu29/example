f36bfeada74829d54577d384a825a6a04b0d3b19
Mario Florence <mflorence1989@gmail.com>
2017-08-07 17:24:32 -0400

    final commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,19 @@
 
+var btn = document.getElementById('mybtn');
+var modal = document.getElementById('mymodal');
+var btnclose = document.getElementById('btnclose');
+
+
+btn.onclick = function(){
+	modal.style.display = 'block';
+}
+	btnclose.onclick = function(){
+		modal.style.display = '';
+	}
+
+
+
+
+setTimeout(function() {
+  modal.style.display = 'block';
+}, 8000);
